0c6cc83e58889845b97dea24823b6735460d27b9
ryuuya <ryuuya0921@gmail.com>
2024-03-04 20:45:40 +0900

    route設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :tasks # ルーティングをまとめて設定(index、new、create、show、edit、update、destroyアクションに対するルーティングを設定してくれる。)
+  root to: 'tasks#index' # トップページの設定
 end
