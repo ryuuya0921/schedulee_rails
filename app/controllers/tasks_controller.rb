da75bf876df10e7efafecf41a0ddfc5ea48ccf95
ryuuya <ryuuya0921@gmail.com>
2024-03-04 21:27:11 +0900

    修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,14 @@
 class TasksController < ApplicationController
-  def index
+  def index #一覧表示画面
     @events = Event.all
   end
+  
+  def new #新規登録画面
+    @event = Event.new
+  end
+  
+  def show #詳細表示画面
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :tasks # ルーティングをまとめて設定(index、new、create、show、edit、update、destroyアクションに対するルーティングを設定してくれる。)
-  root to: 'tasks#index' # トップページの設定
+  root to: 'tasks#index' # トップページの設定にはrootが用いられる
+  
+  get 'tasks/new' => 'tasks/new'
 end
