6e1ef34884d59ef3e449d22b71d37a63ddf0cd29
ryuuya <ryuuya0921@gmail.com>
2024-03-08 20:15:00 +0900

    修正

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,4 @@
-class TasksController < ApplicationController
+class EventsController < ApplicationController
   def index #一覧表示画面
     @events = Event.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :tasks # ルーティングをまとめて設定(index、new、create、show、edit、update、destroyアクションに対するルーティングを設定してくれる。)
-  root to: 'tasks#index' # トップページの設定にはrootが用いられる
+  resources :events # ルーティングをまとめて設定(index、new、create、show、edit、update、destroyアクションに対するルーティングを設定してくれる。)
+  root to: 'events#index' # トップページの設定にはrootが用いられる
   
-  get 'tasks/new' => 'tasks/new'
-  get 'tasks/edit' => 'tasks/edit'
 end
