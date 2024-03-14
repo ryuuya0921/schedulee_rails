0f434e5ff729fc848eda5b0ee79a5a2e471e78a0
ryuuya <ryuuya0921@gmail.com>
2024-03-14 20:52:11 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :events # ルーティングをまとめて設定(index、new、create、show、edit、update、destroyアクションに対するルーティングを設定してくれる。)
-  root to: 'events#index' # トップページの設定にはrootが用いられる
+  get "/" => 'events#index' # トップページの設定にはrootが用いられる
   
 end
