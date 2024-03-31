61f0a400a709d4490203a9480dab58a05c6d595b
ryuuya <ryuuya0921@gmail.com>
2024-04-01 04:04:00 +0900

    カレンダーの修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,15 @@
 class Event < ApplicationRecord
 
+  validate :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date #カレンダーの終了日を開始日以前で設定出来なくする
+
+    if end_date.present? && end_date <= start_date
+      errors.add(:end_date, "は開始日より後の日付でなければなりません")
+    end
+    
+  end
 
 end
